8b77279c3273086976d5ca4a1edcac81880ea6bb
dennis-gomez <dennis.gomez1310@gmail.com>
2026-01-21 11:44:10 -0600

    Implementation of middlewares
    Implementation of body validation on endpoints /bill and /payment, to validate empty fields.

--- notification-service/src/routes/notifications_route.js
+++ notification-service/src/routes/notifications_route.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const { c_notify_user_bill, c_notify_user_payment } = require('../controllers/notifications_controller');
+const { validate_empty_fields } = require('../middlewares/body_error')
 
-router.post('/bill', c_notify_user_bill);
-router.post('/payment', c_notify_user_payment);
+router.post(
+    '/bill', 
+    validate_empty_fields([
+        'bill_code',
+        'bill_issue_date',
+        'notification_receiver',
+        'notification_subject',
+        'reference_id',
+        'notification_message',
+        'notification_reference_type',
+        'details.products',
+        'details.subtotal',
+        'details.total'
+    ]),
+    c_notify_user_bill
+);
+
+router.post(
+    '/payment', 
+    validate_empty_fields([
+        'notification_receiver',
+        'notification_subject',
+        'reference_id',
+        'notification_message',
+        'notification_reference_type',
+        'payment.payment_type',
+        'payment.payment_total',
+        'payment.payment_created_at',
+        'bill.bill_code',
+        'bill.bill_total',
+        'bill.bill_total_paid',
+        'bill.bill_to_pay'
+    ]),
+    c_notify_user_payment
+);
 
 module.exports = router;
