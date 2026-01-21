8cf4376a74c60a3baff2d9a708257c4b5b4919f3
dennis-gomez <dennis.gomez1310@gmail.com>
2026-01-21 11:12:25 -0600

    Clean code
    The app.js and server.js are modify to be more scalable. Delete the code_generator.js because the code of the invoice will be manage in the bill backend. The pdf_service is modify to see better for the user, with the a table invoice implementation. Elimination of redundancy code on email_service.js, use just one service to manage the mail send service. This refactor simplifies the architecture and reduces duplicated logic.

--- notification-service/src/routes/notifications_route.js
+++ notification-service/src/routes/notifications_route.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-const {
-    c_notify_user_bill, 
-    c_notify_user_payment
-} = require('../controllers/notifications_controller');
+const { c_notify_user_bill, c_notify_user_payment } = require('../controllers/notifications_controller');
 
 router.post('/bill', c_notify_user_bill);
-router.post('/payment', c_notify_user_bill);
+router.post('/payment', c_notify_user_payment);
 
 module.exports = router;
--- notification-service/src/services/pdf_service.js
+++ notification-service/src/services/pdf_service.js
@@ -1,53 +1,75 @@
 const PDFDocument = require("pdfkit");
-const unique_code_generator = require("../utils/code_generator");
 
 const generate_bill_PDF = (details) => { 
     return new Promise((resolve, reject) => {
-        const doc = new PDFDocument();
+        const doc = new PDFDocument({ margin: 50 });
         const buffers = [];
-        const today = new Date().toLocaleDateString("es-CR");
 
-        doc
-        .on("data", buffers.push.bind(buffers));
-        doc
-        .on("end", () => {
-            const pdfBuffer = Buffer.concat(buffers);
-            resolve(pdfBuffer);
-        });
+        doc.on("data", buffers.push.bind(buffers));
+        doc.on("end", () => resolve(Buffer.concat(buffers)));
         doc.on("error", reject);
 
         doc
-        .fontSize(18)
-        .text("Corporation Example", { align: "center" });
+        .fontSize(12)
+        .text("Anonymous company", { align: "center" })
+        .moveDown(0.5);
+
+        const y = doc.y;
+        doc
+        .fontSize(10)
+        .text(`Invoice Code: ${details.bill_code}`, 50, y)
+        .text(`Date of Issue: ${details.bill_issue_date}`, 0, y,  { align: "right" });
+
+        doc.moveDown(1.5);
+
+        const tableTop = doc.y;
+        const colProduct = 50;
+        const colQty = 250;
+        const colPrice = 320;
+        const colSubtotal = 420;
 
         doc
-        .moveDown()
-        .fontSize(14)
-        .text(`Invoice Date: ${today}`, { align: "right" });
+        .fontSize(10)
+        .font("Helvetica-Bold")
+        .text('Product', colProduct, tableTop)
+        .text('Quantity', colQty, tableTop, { width: 50, align: 'right' })
+        .text('Price', colPrice, tableTop, { width: 60, align: 'right' })
+        .text('Total', colSubtotal, tableTop, { width: 70, align: 'right' });
 
         doc
-        .moveDown()
-        .fontSize(14)
-        .text(`Code: ${unique_code_generator("INV-")}`, { align: "right" });
+        .moveTo(50, tableTop + 15)
+        .lineTo(545, tableTop + 15)
+        .stroke();
 
-        details.products.forEach(product => {
+        doc.moveDown(1.5)
+
+        doc.font("Helvetica");
+
+        details.details.products.forEach(product => {
+            const y = doc.y+5;
+
+            if (y > doc.page.height - doc.page.margins.bottom - 20) {
+                doc.addPage();
+            }
+
             doc
-            .moveDown()
-            .fontSize(12)
-            .text(
-                `${product.product_name}  ${product.product_quantity} x ${product.product_price} = ${product.product_subtotal}`
-            );
+            .fontSize(10)
+            .text(product.product_name, colProduct, y)
+            .text(product.product_quantity, colQty, y, { width: 50, align: 'right' })
+            .text(product.product_price, colPrice, y, { width: 60, align: 'right' })
+            .text(product.product_subtotal, colSubtotal, y, { width: 70, align: 'right' });
+
+            doc.moveDown();
         });
 
-        doc
-        .moveDown()
-        .fontSize(14)
-        .text(`Subtotal: ${details.subtotal}`, { align: "right" });
+        doc.moveDown(1.5);
 
         doc
-        .moveDown()
-        .fontSize(14)
-        .text(`Total: ${details.total}`, { align: "right" });
+        .font("Helvetica-Bold")
+        .text(`Subtotal: ${details.details.subtotal}`, 0, doc.y, { align: "right" })
+        .moveDown(0.5)
+        .fontSize(12)
+        .text(`Total: ${details.details.total}`, { align: "right" });
 
         doc.end();
 
@@ -60,48 +82,50 @@ const generate_payment_PDF = (payment_details, bill_details) => {
         const doc = new PDFDocument();
         const buffers = [];
 
-        doc
-        .on("data", buffers.push.bind(buffers));
-        doc
-        .on("end", () => {
-            const pdfBuffer = Buffer.concat(buffers);
-            resolve(pdfBuffer);
-        });
+        doc.on("data", buffers.push.bind(buffers));
+        doc.on("end", () => resolve(Buffer.concat(buffers)));
         doc.on("error", reject);
 
         doc
-        .fontSize(18)
-        .text("Corporation Example", { align: "center" });
+        .fontSize(12)
+        .text("Anonymous company", { align: "center" })
+        .moveDown(0.5);
+
+        const y = doc.y;
+        doc
+        .fontSize(10)
+        .text(`Invoice Code: ${bill_details.bill_code}`, 50, y)
+        .text(`Date of Issue: ${payment_details.payment_created_at}`, 0, y,  { align: "right" });
 
         doc
         .moveDown()
-        .fontSize(14)
-        .text(`Payment Date: ${payment_details.payment_created_at}`, { align: "right" });
+        .fontSize(12)
+        .text(`Payment Type:  ${payment_details.payment_type}`, { align: "center" });
 
         doc
         .moveDown()
-        .fontSize(14)
-        .text(`Invoice Code: ${bill_details.bill_code}`, { align: "right" });
+        .fontSize(12)
+        .text(`Total Invoice:  ${bill_details.bill_total}`, { align: "center" });
 
         doc
         .moveDown()
-        .fontSize(14)
-        .text(`Payment Type: ${payment_details.payment_type}`, { align: "right" });
+        .fontSize(12)
+        .text(`Total Paid:  ${payment_details.payment_total}`, { align: "center" });
 
         doc
         .moveDown()
-        .fontSize(14)
-        .text(`Total Payment: ${payment_details.payment_total}`, { align: "right" });
+        .fontSize(12)
+        .text(`Total Payment:  ${bill_details.bill_total_paid}`, { align: "center" });
+
+        doc
+        .moveTo(50, y + 15)
+        .lineTo(545, y + 15)
+        .stroke();
 
         doc
         .moveDown()
-        .fontSize(14)
-        .text(`Total Invoice: ${bill_details.bill_total}`, { align: "right" });
-
-        doc
-        .moveDown()
-        .fontSize(14)
-        .text(`Total Paid: ${bill_details.bill_total_paid}`, { align: "right" });
+        .fontSize(15)
+        .text(`Total Remaining:  ${bill_details.bill_to_pay}`, { align: "center" });
 
         doc.end();
 
